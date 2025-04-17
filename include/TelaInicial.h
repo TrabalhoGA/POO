531579f5a15f72977fff6c2335cc07015826ef48
Lucas Eduardo Jaguezeski <lucasjaguezeski@gmail.com>
2025-04-17 16:54:38 -0300

    Correção do commit anterior

--- include/TelaInicial.h
+++ include/TelaInicial.h
@@ -1,1 +1,21 @@
 #pragma once
+#ifndef TELAINICIAL_H
+#define TELAINICIAL_H
+
+#include "Jogo.h"
+
+using namespace std;
+
+class TelaInicial : public TelaEstado {
+public:
+	TelaInicial(Jogo* jogo);
+	~TelaInicial();
+
+	void exibirTela() override;
+	void handleInput(int input) override;
+
+private:
+	Jogo* jogo;
+};
+
+#endif // TELAINICIAL_H
